1155356f702b5afdf4a9abc9cf32aa89a802ef04
Christina Woodhams <cwoodhams89@gmail.com>
2022-11-02 08:53:50 -0400

    Testing middleware.

--- server/server.js
+++ server/server.js
@@ -5,21 +5,24 @@ const path = require("path"); //connects server to html
 const app = express();
 app.use(cors());
 app.use(express.json()); 
+// Middleware
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/linda.png"));
-})
+// // End points (Choose this or Middleware)
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "../public/linda.png"));
+// })
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-})
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// })
 
-app.get("/styles", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-})
+// app.get("/styles", function(req, res){
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// })
 
-const port = process.env.PORT || 4005; //variable to store the port -- supplied by deployment service, for ex: Heroku. 4005 allows you to run the code locally prior to deployment. 
+// const port = process.env.PORT || 4005; //variable to store the port -- supplied by deployment service, for ex: Heroku. 4005 allows you to run the code locally prior to deployment. 
 
-app.listen(port, ()=> {
-    console.log(`We vibin on port ${port}`);
-});
+// app.listen(port, ()=> {
+//     console.log(`We vibin on port ${port}`);
+// });

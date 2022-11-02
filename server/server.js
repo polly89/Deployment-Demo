1cd3dca6f02c63cd014059cd1e8102b9e608a3b3
Christina Woodhams <cwoodhams89@gmail.com>
2022-11-02 07:32:37 -0400

    Set up style page for Heroku.

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ app.use(cors());
 app.use(express.json()); 
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+})
+
+app.get("/styles", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/index.css"));
 })
 
 const port = process.env.PORT || 4005; //variable to store the port -- supplied by deployment service, for ex: Heroku. 4005 allows you to run the code locally prior to deployment. 

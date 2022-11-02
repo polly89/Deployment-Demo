5c1e49ce0a52aa990de659f8ab199a9a63bcf127
Christina Woodhams <cwoodhams89@gmail.com>
2022-11-02 07:38:04 -0400

    Forth commit. Added app.get for the images in the public folder.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "../pubic"));
+})
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })

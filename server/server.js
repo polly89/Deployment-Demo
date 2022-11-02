6c9f326f0d78432f6acd771ef9f4a104641d056e
Christina Woodhams <cwoodhams89@gmail.com>
2022-11-02 08:16:57 -0400

    confirmed path

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json()); 
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public"));
+    res.sendFile(path.join(__dirname, "../public/linda.png"));
 })
 
 app.get("/", function(req, res) {
